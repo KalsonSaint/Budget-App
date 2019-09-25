a645cd5630cbfac8adce4ecda1574a105243607f
KalsonSaint <saintkalson@gmail.com>
2019-09-25 13:54:35 +0100

    Updated Budget App

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ const budgetController = (() => {
 })();
 
 const UIController = (() => {
+    let DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
     return {
         getInput: () => {
             return {
-                type: document.querySelector('.add__type').value,  // either inc or exp
-                descriiption: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMStrings.inputType).value,  // either inc or exp
+                descriiption: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
             };
+        },
+        getDOMString: () => {
+            return DOMStrings;
         }
     };
 })();
 
 const appController = ((budgetCtrl, UICtrl) => {
+    const setEventListeners = () => {
+        let DOM = UICtrl.getDOMString();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', (e)=>{
+            if(e.keycode === 13 || e.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
 
     const ctrlAddItem = () => {
          // Get field input data
@@ -27,12 +45,5 @@ const appController = ((budgetCtrl, UICtrl) => {
  
         // Calculate the budget
 
-    }
-
-   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-   document.addEventListener('keypress', (e)=>{
-    if(e.keycode === 13 || e.which === 13){
-        ctrlAddItem();
-    }
-   });
+    };
 })(budgetController, UIController);
