34b5b0d1f6bd8600fb7e8e6b404129ba1da0ccd5
KalsonSaint <saintkalson@gmail.com>
2019-09-27 11:01:04 +0100

    Budget App Updated

--- app.js
+++ app.js
@@ -1,49 +1,104 @@
 const budgetController = (() => {
-    
+  const Expense = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const Income = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  let data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: (type, des, val) => {
+      let newItem, ID;
+      //   Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      //   Create new item 'inc' or 'exp'
+      if (newItem === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (newItem === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+      //   push it to data structure
+      data.allItems[type].push(newItem);
+
+      //   return new element
+      return newItem;
+    },
+    testing: () => {
+      console.log(data);
+    }
+  };
 })();
 
 const UIController = (() => {
-    let DOMStrings = {
-        inputType: '.add__type',
-        inputDescription: '.add__description',
-        inputValue: '.add__value',
-        inputBtn: '.add__btn'
+  let DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // either inc or exp
+        descriiption: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMString: () => {
+      return DOMStrings;
     }
-    return {
-        getInput: () => {
-            return {
-                type: document.querySelector(DOMStrings.inputType).value,  // either inc or exp
-                descriiption: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value
-            };
-        },
-        getDOMString: () => {
-            return DOMStrings;
-        }
-    };
+  };
 })();
 
 const appController = ((budgetCtrl, UICtrl) => {
-    const setEventListeners = () => {
-        let DOM = UICtrl.getDOMString();
-        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-        document.addEventListener('keypress', (e)=>{
-            if(e.keycode === 13 || e.which === 13){
-                ctrlAddItem();
-            }
-        });
-    };
+  const setEventListeners = () => {
+    let DOM = UICtrl.getDOMString();
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+    document.addEventListener("keypress", e => {
+      if (e.keycode === 13 || e.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
-    const ctrlAddItem = () => {
-         // Get field input data
-        let input = UICtrl.getInput();
-        console.log(input);
-        
-        // Add item to budget controller
+  const ctrlAddItem = () => {
+    let input, newItem;
+    // Get field input data
+    input = UICtrl.getInput();
 
-        //  Add item to UI
- 
-        // Calculate the budget
+    // Add item to budget controller
+    newItem = budgetCtrl.addItem(input.type, input.des, input.value);
+    //  Add item to UI
 
-    };
+    // Calculate the budget
+  };
+
+  return {
+    init: () => {
+      console.log("Application Started...");
+      setEventListeners();
+    }
+  };
 })(budgetController, UIController);
+
+appController.init();
