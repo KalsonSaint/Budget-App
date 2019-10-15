5f2470b2f38843cf46f98d4a4e7271f700086d77
KalsonSaint <saintkalson@gmail.com>
2019-10-16 00:05:27 +0100

    Debugging

--- app.js
+++ app.js
@@ -64,6 +64,19 @@ const UIController = (() => {
         value: document.querySelector(DOMStrings.inputValue).value
       };
     },
+    addListItem: (obj, type) => {
+      // create html string with placeholder text
+      '<div class="item clearfix" id="income-0">
+          <div class="item__description">Salary</div>
+          <div class="right clearfix">
+              <div class="item__value">+ 2,100.00</div>
+              <div class="item__delete">
+                  <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+              </div>
+          </div>
+      </div>'
+      // replace placeholder text with data
+    },
     getDOMString: () => {
       return DOMStrings;
     }
